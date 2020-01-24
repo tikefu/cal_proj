68c8a1ea21a7b78d17fb5caf5bb047a90b8d10da
tikefu <59137711+tikefu@users.noreply.github.com>
2020-01-24 22:07:29 +0600

    Add files via upload
    Here it is

---
+++ togit.py
@@ -0,0 +1,18 @@
+typename = input('name: ')
+
+typelastname = input('type last name: ')
+
+age = input("what's your age? ")
+
+idinput = int(input("typeid: "))
+
+if idinput == 1:
+    print("Микроволновка LGцена: 45000 тг")
+    idinput =int(input("typeid: "))
+if idinput == 2:
+    print("Телевизор, цена 80000 тг ")
+    idinput = int(input("typeid: "))
+if idinput == 3:
+    print("Компьютер, цена 220000 тг ")
+    idinput = int(input("typeid: "))
+
